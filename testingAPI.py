aaf81d54d5ed9e0d75b91624ab77b090920904ea
Nearo007 <araujo8166@gmail.com>
2024-10-18 15:44:59 -0300

    using API without postman

---
+++ testingAPI.py
@@ -0,0 +1,14 @@
+import requests
+
+excelFilePath = "data//excel_file.xlsx"
+url = "http://127.0.0.1:5000/upload-excel"
+
+try:
+    with open(excelFilePath, 'rb') as file:
+        files = { 'file': file}
+        response = requests.post(url, files=files)
+
+    print(response.json())
+
+except Exception as e:
+    print('\n', e)
